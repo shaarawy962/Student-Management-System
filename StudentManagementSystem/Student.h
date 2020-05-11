3a53990ed3e564018d7d351763e80a7ba5f4233e
Shaarawy <mhshaarawy3@outlook.com>
2020-05-12 01:24:47 +0200

    Inserting students and subjects features

--- StudentManagementSystem/Student.h
+++ StudentManagementSystem/Student.h
@@ -10,9 +10,239 @@ private:
 	std::string Name;
 	std::string dept;
 	int numberOfCourses;
+	SubjectList* Courses;
+	Student* nextStud;
 
 public:
 
+	Student()
+	{
+		Name = "";
+		dept = "";
+		numberOfCourses = 0;
+		Courses = new SubjectList;
+		nextStud = nullptr;
+	}
+
+	Student(std::string name, std::string department, int numOfCourses)
+	{
+		Name = name;
+		dept = department;
+		numberOfCourses = numOfCourses;
+		nextStud = NULL;
+	}
+
+	friend std::istream& operator >>(std::istream& in, Student& param)
+	{
+		std::getline(in, param.Name);
+		in >> param.dept;
+		in >> param.numberOfCourses;
+		return in;
+	}
+
+	SubjectList* GetCourses()
+	{
+		return this->Courses;
+	}
 
 	friend class StudentList;
+};
+
+class StudentList
+{
+private:
+	Student* DummyNode;
+	Student* head;
+	Student* tail;
+	unsigned int Size;
+
+public:
+	class iterator
+	{
+	private:
+		Student* currentNode;
+		friend class Student;
+	public:
+		iterator()
+		{
+			currentNode = NULL;
+		}
+
+		iterator(Student* myNode)
+		{
+			currentNode = myNode;
+		}
+
+		iterator operator++()
+		{
+			if (currentNode == NULL)
+				throw std::out_of_range("cannot increase bigger than list size");
+			currentNode = currentNode->nextStud;
+			return *this;
+		}
+
+		Student& operator *()
+		{
+			return *currentNode;
+		}
+
+		bool operator ==(iterator it)
+		{
+			if (it.currentNode == this->currentNode)
+				return true;
+			return false;
+		}
+
+		bool operator !=(iterator it)
+		{
+			if (it.currentNode != this->currentNode)
+				return true;
+			return false;
+		}
+
+		void operator =(iterator it)
+		{
+			this->currentNode = it.currentNode;
+		}
+
+		void operator =(Student* currNode)
+		{
+			this->currentNode = currNode;
+		}
+
+	protected:
+
+	};
+
+	StudentList()
+	{
+		head = NULL;
+		tail = NULL;
+		Size = 0;
+		DummyNode = NULL;
+	}
+
+	void append(Student* param)
+	{
+		
+		
+		if (head == NULL && tail == NULL) {
+			head = param;
+			tail = param;
+			tail->nextStud = DummyNode;
+		}
+
+		//[] -> = tail & head
+		//[1] -> = tail, prev = prevTail
+		//[1, 2] -> = tail, prev = prevTail
+
+		else
+		{
+			tail->nextStud = param;
+			tail = param;
+			tail->nextStud = DummyNode;
+		}
+		++Size;
+	}
+
+	/*void erase(iterator position)
+	{
+		if (position == end())
+			throw std::out_of_range("Trying to erase outside the list");
+
+		else
+		{
+			if (position == begin())
+			{
+				head = head->Next;
+				delete head->Prev;
+			}
+
+			else if (position == --end())
+			{
+				tail = tail->Prev;
+				delete tail->Next;
+			}
+
+			else
+			{
+				iterator current = begin();
+				while (current != position)
+				{
+					++current;
+				}
+
+				Node<Type>* currNode = &*current;
+				Node<Type>* prevNode = currNode->Prev;
+
+				prevNode->Next = currNode->Next;
+				currNode->Next->Prev = prevNode;
+				delete currNode;
+			}
+		}
+		--Size;
+	}*/
+
+	int getSize()
+	{
+		return Size;
+	}
+
+	//~StudentList()
+	//{
+	//	Student* temp = new Node<Type>();
+	//	temp = head;
+
+	//	while (head != NULL) {
+	//		// [1, 2, 3, 4]
+	//		// []
+	//		temp = head->Next;
+	//		delete head;
+	//		head = temp;
+	//	}
+
+	//	tail = head = DummyNode = NULL;
+	//	delete tail;
+	//	delete head;
+	//	delete DummyNode;
+	//	delete temp;
+	//	Size = 0;
+	//}
+
+	iterator begin()
+	{
+		return iterator(head);
+	}
+
+	iterator end()
+	{
+		return iterator(DummyNode);
+	}
+
+	/*friend std::ostream& operator << (std::ostream& out, StudentList& myList)
+	{
+		Student* CurrentNode = new Node<Type>();
+		CurrentNode = myList.head;
+		for (int i = 0; i < myList.Size; i++)
+		{
+			out << *CurrentNode;
+			CurrentNode = CurrentNode->Next;
+		}
+		return out;
+	}*/
+
+	void AddStudent()
+	{
+		Student* curr = new Student();
+		std::cin >> *curr;
+		append(curr);
+		std::cin.ignore(1000, '\n');
+	}
+
+	Student* getHead()
+	{
+		return head;
+	}
+
+	protected:
 };
--- StudentManagementSystem/Subject.h
+++ StudentManagementSystem/Subject.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 class SubjectList;
 
 class Subject
@@ -41,21 +42,229 @@ public:
 	}
 
 	friend class SubjectList;
+
+	friend std::istream& operator >>(std::istream& in, Subject& param)
+	{
+		std::getline(in, param.subjectName);
+		in >> param.gradePointAverage;
+		in >> param.grade;
+		in >> param.total;
+		return in;
+	}
 };
+
+//class SubjectList
+//{
+//private:
+//	Subject* head;
+//	Subject* tail;
+//	
+//	void insert()
+//	{
+//
+//	}
+//public:
+//
+//	SubjectList()
+//	{
+//		head = nullptr;
+//	}
+//
+//	void AddSubject()
+//	{
+//
+//	}
+//};	
 
 class SubjectList
 {
 private:
+	Subject* DummyNode;
 	Subject* head;
+	Subject* tail;
+	unsigned int Size;
 
 public:
+	class iterator
+	{
+	private:
+		Subject* currentNode;
+		friend class Subject;
+	public:
+		iterator()
+		{
+			currentNode = NULL;
+		}
+
+		iterator(Subject* myNode)
+		{
+			currentNode = myNode;
+		}
+
+		iterator operator++()
+		{
+			if (currentNode == NULL)
+				throw std::out_of_range("cannot increase bigger than list size");
+			currentNode = currentNode->next;
+			return *this;
+		}
+
+		Subject& operator *()
+		{
+			return *currentNode;
+		}
+
+		bool operator ==(iterator it)
+		{
+			if (it.currentNode == this->currentNode)
+				return true;
+			return false;
+		}
+
+		bool operator !=(iterator it)
+		{
+			if (it.currentNode != this->currentNode)
+				return true;
+			return false;
+		}
+
+		void operator =(iterator it)
+		{
+			this->currentNode = it.currentNode;
+		}
+
+		void operator =(Subject* currNode)
+		{
+			this->currentNode = currNode;
+		}
+
+	protected:
+
+	};
 
 	SubjectList()
 	{
-		head = nullptr;
+		head = NULL;
+		tail = NULL;
+		Size = 0;
+		DummyNode = NULL;
 	}
 
-	SubjectList(int numOfSubjects)
+	void append(Subject* param)
 	{
+
+
+		if (head == NULL && tail == NULL) {
+			head = param;
+			tail = param;
+			tail->next = DummyNode;
+		}
+
+		//[] -> = tail & head
+		//[1] -> = tail, prev = prevTail
+		//[1, 2] -> = tail, prev = prevTail
+
+		else
+		{
+			tail->next = param;
+			tail = param;
+			tail->next = DummyNode;
+		}
+		++Size;
 	}
+
+	/*void erase(iterator position)
+	{
+		if (position == end())
+			throw std::out_of_range("Trying to erase outside the list");
+
+		else
+		{
+			if (position == begin())
+			{
+				head = head->Next;
+				delete head->Prev;
+			}
+
+			else if (position == --end())
+			{
+				tail = tail->Prev;
+				delete tail->Next;
+			}
+
+			else
+			{
+				iterator current = begin();
+				while (current != position)
+				{
+					++current;
+				}
+
+				Node<Type>* currNode = &*current;
+				Node<Type>* prevNode = currNode->Prev;
+
+				prevNode->Next = currNode->Next;
+				currNode->Next->Prev = prevNode;
+				delete currNode;
+			}
+		}
+		--Size;
+	}*/
+
+	int getSize()
+	{
+		return Size;
+	}
+
+	//~SubjectList()
+	//{
+	//	Subject* temp = new Node<Type>();
+	//	temp = head;
+
+	//	while (head != NULL) {
+	//		// [1, 2, 3, 4]
+	//		// []
+	//		temp = head->Next;
+	//		delete head;
+	//		head = temp;
+	//	}
+
+	//	tail = head = DummyNode = NULL;
+	//	delete tail;
+	//	delete head;
+	//	delete DummyNode;
+	//	delete temp;
+	//	Size = 0;
+	//}
+
+	iterator begin()
+	{
+		return iterator(head);
+	}
+
+	iterator end()
+	{
+		return iterator(DummyNode);
+	}
+
+	/*friend std::ostream& operator << (std::ostream& out, SubjectList& myList)
+	{
+		Subject* CurrentNode = new Node<Type>();
+		CurrentNode = myList.head;
+		for (int i = 0; i < myList.Size; i++)
+		{
+			out << *CurrentNode;
+			CurrentNode = CurrentNode->Next;
+		}
+		return out;
+	}*/
+
+	void AddSubject()
+	{
+		Subject* curr = new Subject();
+		std::cin >> *curr;
+		append(curr);
+	}
+
+protected:
 };
